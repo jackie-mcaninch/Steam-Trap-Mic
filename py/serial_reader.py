499b6d73a1d4294dd8613d5e5d5ae149a3912f20
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2022-10-30 17:08:35 -0500

    py: updating to show peak to peak

--- py/serial_reader.py
+++ py/serial_reader.py
@@ -1,19 +1,36 @@
 import serial
 import matplotlib.pyplot as plt
 import sys
+import statistics
+import time
 
-# USAGE: python3 py/serial_reader.py <COM_PORT> <NUM_POINTS>
+# USAGE: python3 py/serial_reader.py <COM_PORT> <NUM_SECS> <NUM_POINTS>
 
 com_port = sys.argv[1]
-num_pts = sys.argv[2] if len(sys.argv) > 2 else 1000
+num_secs = float(sys.argv[2]) if len(sys.argv) > 2 else 5
+num_pts = int(sys.argv[3]) if len(sys.argv) > 3 else 100*num_secs
+resolution = num_secs / num_pts
 ser = serial.Serial(com_port, 115200, timeout=1)
 data = []
-counter = 0
+hist_threshold = 100
+peak_diffs = []
+devs = []
+peak_threshold = 3.3
 
-while counter < num_pts:
+start = time.time()
+t = time.time() - start
+while t < num_secs:
     datum = float(ser.readline().decode('utf-8').strip())
-    data.append(datum)
-    counter += 1
+    data.append((datum, t))
+    if len(data) >= hist_threshold:
+        latest = [d for d,t in data[-hist_threshold:]]
+        peak_diffs.append((max(latest)-min(latest), t))
+        devs.append((statistics.stdev(latest), t))
+    t = time.time() - start
 
-plt.plot(range(num_pts), data, color="blue")
+plt.xlabel("Time since start (s)")
+plt.ylabel("Voltage")
+plt.plot([t for (d,t) in data], [d for (d,t) in data], color="blue")
+plt.plot([t for (p,t) in peak_diffs], [p for (p,t) in peak_diffs], color="red")
+plt.plot([t for (s,t) in devs], [s for (s,t) in devs], color="green")
 plt.show()
