2fb80057eef6502662b69d7d02bebdf9031cc7c1
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2023-03-25 16:24:31 -0500

    updating helper scripts

--- py/serial_reader.py
+++ py/serial_reader.py
@@ -1,38 +1,38 @@
-import serial
+import argparse
 import matplotlib.pyplot as plt
-import sys
+import serial
 import time
 
-# USAGE: python3 py/serial_reader.py <COM_PORT> <NUM_SECS> <NUM_POINTS>
+# USAGE: python3 py/serial_reader.py <COM_PORT> -d <NUM_SECS>
 
-com_port = sys.argv[1]
-num_secs = float(sys.argv[2]) if len(sys.argv) > 2 else 5
-num_pts = int(sys.argv[3]) if len(sys.argv) > 3 else 100*num_secs
-resolution = num_secs / num_pts
+# read command line arguments
+parser = argparse.ArgumentParser(description="Plot the values received from Moteino over serial.")
+parser.add_argument("COM_PORT", type=str, nargs=1, help="Designated COM port to listen on (i.e. COM5 on Windows)")
+parser.add_argument("-d, --duration", dest="num_secs", type=int, nargs=1, default=5, help="Number of seconds to listen for serial data")
+args = parser.parse_args()
+
+# initialize serial
+com_port = args.com_port[0]
 ser = serial.Serial(com_port, 115200, timeout=1)
-data = []
-hist_threshold = 100
-peak_diffs = []
-devs = []
-peak_threshold = 3.3
+ser.readline()
 
+# start loop
+num_secs = args.num_secs
 start = time.time()
 t = time.time() - start
+data = []
 datum = 0
-ser.readline()
 while t < num_secs:
+    # try to get a reading
     try:
         datum = float(ser.readline().decode('utf-8').strip())
-    except ValueError: pass # just use last recorded datum
+    # use last recorded datum if nothing received
+    except ValueError: pass
     data.append((datum, t))
-    # if len(data) >= hist_threshold:
-    #     latest = [d for d,t in data[-hist_threshold:]]
-    #     peak_diffs.append((max(latest)-min(latest), t))
     t = time.time() - start
 
+# plot the voltage data received vs time
 plt.xlabel("Time since start (s)")
 plt.ylabel("Voltage")
 plt.plot([t for (d,t) in data], [d for (d,t) in data], color="blue")
-# plt.plot([t for (p,t) in peak_diffs], [p for (p,t) in peak_diffs], color="red")
-# plt.plot([t for (s,t) in devs], [s for (s,t) in devs], color="green")
 plt.show()
